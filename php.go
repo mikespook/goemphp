b1b3cc0d92746b4ef96cbf2b02b30cb306e10dc4
Xing Xing <mikespook@gmail.com>
2015-09-04 11:29:15 +1200

    refactoring

--- php.go
+++ php.go
@@ -4,127 +4,114 @@
 
 package php
 
-// #cgo CFLAGS: -I/usr/include/php5 -I/usr/include/php5/main -I/usr/include/php5/TSRM -I/usr/include/php5/Zend -I/usr/include/php5/ext -I/usr/include/php5/
-// #cgo LDFLAGS: -lphp5
-// #include "zend.h"
-// void php_set_ini(char *ini);
-// void php_startup();
-// char * php_exec_file(char *filename);
-// char * php_eval_script(char *script);  
-// void php_shutdown(void);
-// void php_add_var_str(char *varname, char *value);
-// void php_add_var_double(char *varname, double value);
-// void php_add_var_long(char *varname, long value);
-// void php_add_var_bool(char *varname, int value);
-// zval * php_array_init();
-// void php_array_add(zval *arr, char *key, char *value);
-// void php_array_end(zval *arr, char *varname);
-// void php_unset(char *varname);
+//go:generate ./gen.sh
+
+// #include "php_embed.h"
 import "C"
 
 import (
-    "os"
-    "errors"
-    "syscall"
-    "reflect"
+	"errors"
+	"os"
+	"reflect"
+	"syscall"
 )
 
 const (
-    Success = 0
-    Failure = -1
+	Success = 0
+	Failure = -1
 )
 
 var (
-    ErrInvalidType = errors.New("Invalide type")
-    ErrInvalidValue = errors.New("Invalide value")
+	ErrInvalidType  = errors.New("Invalide type")
+	ErrInvalidValue = errors.New("Invalide value")
 )
 
 type PHP struct {
-    stdout, stderr *os.File
-    inifile string
+	stdout, stderr *os.File
+	inifile        string
 }
 
 func New() (php *PHP) {
-    php = &PHP{
-        stdout: os.Stdout,
-        stderr: os.Stderr,
-    }
-    return
+	php = &PHP{
+		stdout: os.Stdout,
+		stderr: os.Stderr,
+	}
+	return
 }
 
 func (php *PHP) Unset(name string) {
-    C.php_unset(C.CString(name))
+	C.php_unset(C.CString(name))
 }
 
 func (php *PHP) Array(name string, value map[string]string) {
-    z := C.php_array_init()
-    for k, v := range value {
-        C.php_array_add(z, C.CString(k), C.CString(v))
-    }
-    C.php_array_end(z, C.CString(name))
+	z := C.php_array_init()
+	for k, v := range value {
+		C.php_array_add(z, C.CString(k), C.CString(v))
+	}
+	C.php_array_end(z, C.CString(name))
 }
 
 func (php *PHP) Var(name string, value interface{}) (err error) {
-    t := reflect.TypeOf(value)
-    switch t.Kind() {
-    case reflect.Bool:
-        var b C.int
-        if value.(bool) {
-            b = 1
-        } else {
-            b = 0
-        }
-        C.php_add_var_bool(C.CString(name), b)
-    case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int8,
-        reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint8:
-        C.php_add_var_long(C.CString(name), C.long(value.(int)))
-    case reflect.Float32, reflect.Float64:
-        C.php_add_var_double(C.CString(name), C.double(value.(float64)))
-    case reflect.String:
-        C.php_add_var_str(C.CString(name), C.CString(value.(string)))
-    default:
-        err = ErrInvalidType
-    }
-    return
+	t := reflect.TypeOf(value)
+	switch t.Kind() {
+	case reflect.Bool:
+		var b C.int
+		if value.(bool) {
+			b = 1
+		} else {
+			b = 0
+		}
+		C.php_add_var_bool(C.CString(name), b)
+	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int8,
+		reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint8:
+		C.php_add_var_long(C.CString(name), C.long(value.(int)))
+	case reflect.Float32, reflect.Float64:
+		C.php_add_var_double(C.CString(name), C.double(value.(float64)))
+	case reflect.String:
+		C.php_add_var_str(C.CString(name), C.CString(value.(string)))
+	default:
+		err = ErrInvalidType
+	}
+	return
 }
 
 func (php *PHP) Stdout(f *os.File) {
-    php.stdout = f
+	php.stdout = f
 }
 
 func (php *PHP) Stderr(f *os.File) {
-    php.stderr = f
+	php.stderr = f
 }
 
 func (php *PHP) IniFile(ini string) {
-    php.inifile = ini
+	php.inifile = ini
 }
 
 func (php *PHP) Startup() {
-    syscall.Dup2(int(php.stdout.Fd()), 1)
-    syscall.Dup2(int(php.stderr.Fd()), 2)
-    C.php_set_ini(C.CString(php.inifile))
-    C.php_startup()
+	syscall.Dup2(int(php.stdout.Fd()), 1)
+	syscall.Dup2(int(php.stderr.Fd()), 2)
+	C.php_set_ini(C.CString(php.inifile))
+	C.php_startup()
 }
 
-func (php * PHP) Exec(filepath string) (err error) {
-    if _, err = os.Stat(filepath); err != nil {
-        return
-    }
-    if err := C.php_exec_file(C.CString(filepath)); err != nil {
-        return errors.New(C.GoString(err))
-    }
-    return
-    //return php.Eval("require('" + filepath + "');")
+func (php *PHP) Exec(filepath string) (err error) {
+	if _, err = os.Stat(filepath); err != nil {
+		return
+	}
+	if err := C.php_exec_file(C.CString(filepath)); err != nil {
+		return errors.New(C.GoString(err))
+	}
+	return
+	//return php.Eval("require('" + filepath + "');")
 }
 
-func (php * PHP) Eval(script string) (err error) {
-    if err := C.php_eval_script(C.CString(script)); err != nil {
-        return errors.New(C.GoString(err))
-    }
-    return
+func (php *PHP) Eval(script string) (err error) {
+	if err := C.php_eval_script(C.CString(script)); err != nil {
+		return errors.New(C.GoString(err))
+	}
+	return
 }
 
-func (php * PHP) Close() {
-    C.php_shutdown()
+func (php *PHP) Close() {
+	C.php_shutdown()
 }
--- php_test.go
+++ php_test.go
@@ -5,164 +5,164 @@
 package php
 
 import (
-    "os"
-    "io/ioutil"
-    "testing"
+	"io/ioutil"
+	"os"
+	"testing"
 )
 
-const(
-    FileName = "test.php"
-    TestFileName = "test.txt"
+const (
+	FileName     = "test.php"
+	TestFileName = "test.txt"
 )
 
 var (
-    php *PHP
+	php *PHP
 )
 
 func init() {
-    php = New()
-    php.Startup()
+	php = New()
+	php.Startup()
 }
 
 func TestExec(t *testing.T) {
-    if err := ioutil.WriteFile(FileName, []byte("<?php echo 'TestExec\n';file_put_contents('test.txt', 'abcdef');"), 0644); err != nil {
-        t.Errorf("ioutil.WriteFile: %s", err)
-    }
-    defer func() {
-        if err := os.Remove(FileName); err != nil {
-            t.Errorf("os.Remove: %s", err)
-        }
-        if err := os.Remove(TestFileName); err != nil {
-            t.Errorf("os.Remove: %s", err)
-        }
-    }()
-    if err := php.Exec(FileName); err != nil {
-        t.Errorf("php.Exec: %s", err)
-    }
+	if err := ioutil.WriteFile(FileName, []byte("<?php echo 'TestExec\n';file_put_contents('test.txt', 'abcdef');"), 0644); err != nil {
+		t.Errorf("ioutil.WriteFile: %s", err)
+	}
+	defer func() {
+		if err := os.Remove(FileName); err != nil {
+			t.Errorf("os.Remove: %s", err)
+		}
+		if err := os.Remove(TestFileName); err != nil {
+			t.Errorf("os.Remove: %s", err)
+		}
+	}()
+	if err := php.Exec(FileName); err != nil {
+		t.Errorf("php.Exec: %s", err)
+	}
 
-    if _, err := os.Stat(TestFileName); err != nil {
-        t.Errorf("php::file_put_contents: %s", err)
-    }
+	if _, err := os.Stat(TestFileName); err != nil {
+		t.Errorf("php::file_put_contents: %s", err)
+	}
 }
 
 func TestExecErr(t *testing.T) {
-    if err := ioutil.WriteFile(FileName, []byte("<?php echo TestExec"), 0644); err != nil {
-        t.Errorf("ioutil.WriteFile: %s", err)
-    }
-    defer func() {
-        if err := os.Remove(FileName); err != nil {
-            t.Errorf("os.Remove: %s", err)
-        }
-    }()
-    if err := php.Exec(FileName); err == nil {
-        t.Errorf("php.Exec should have a panic.")
-    }
+	if err := ioutil.WriteFile(FileName, []byte("<?php echo TestExec"), 0644); err != nil {
+		t.Errorf("ioutil.WriteFile: %s", err)
+	}
+	defer func() {
+		if err := os.Remove(FileName); err != nil {
+			t.Errorf("os.Remove: %s", err)
+		}
+	}()
+	if err := php.Exec(FileName); err == nil {
+		t.Errorf("php.Exec should have a panic.")
+	}
 }
 
 func TestExecNotFound(t *testing.T) {
-    if err := php.Exec("not-found.php"); err == nil {
-        t.Errorf("php.Exec should have a panic.")
-    }
+	if err := php.Exec("not-found.php"); err == nil {
+		t.Errorf("php.Exec should have a panic.")
+	}
 }
 
 func TestEval(t *testing.T) {
-    if err := php.Eval("echo 'TestEval\n';"); err != nil {
-        t.Errorf("php.Eval: %s", err)
-    }
+	if err := php.Eval("echo 'TestEval\n';"); err != nil {
+		t.Errorf("php.Eval: %s", err)
+	}
 }
 
 func TestEvalThrowExp(t *testing.T) {
-    if err := php.Eval("throw new Exception('test exception');"); err == nil {
-        t.Errorf("php.Eval should have a panic.")
-    }
+	if err := php.Eval("throw new Exception('test exception');"); err == nil {
+		t.Errorf("php.Eval should have a panic.")
+	}
 }
 
 func TestEvalTriggerError(t *testing.T) {
-    if err := php.Eval("trigger_error('test error');"); err == nil {
-        t.Errorf("php.Eval should have a panic.")
-    }
+	if err := php.Eval("trigger_error('test error');"); err == nil {
+		t.Errorf("php.Eval should have a panic.")
+	}
 }
 
 func TestEvalErr(t *testing.T) {
-    if err := php.Eval("echo ;"); err == nil {
-        t.Errorf("php.Eval should have a panic.")
-    }
+	if err := php.Eval("echo ;"); err == nil {
+		t.Errorf("php.Eval should have a panic.")
+	}
 }
 
 func TestVar(t *testing.T) {
-    if err := php.Var("v", "test"); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Var("v", "test"); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 
-    if err := php.Eval("var_dump($v);"); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Eval("var_dump($v);"); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 
-    if err := php.Var("v", true); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Var("v", true); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 
-    if err := php.Eval("var_dump($v);"); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Eval("var_dump($v);"); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 
-    if err := php.Var("v", 123); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Var("v", 123); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 
-    if err := php.Eval("var_dump($v);"); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Eval("var_dump($v);"); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 
-    if err := php.Var("v", 123.456); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Var("v", 123.456); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 
-    if err := php.Eval("var_dump($v);"); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Eval("var_dump($v);"); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 }
 
 func TestArray(t *testing.T) {
-    php.Array("v", map[string]string{"a":"z", "b":"y", "c":"x"})
+	php.Array("v", map[string]string{"a": "z", "b": "y", "c": "x"})
 
-    if err := php.Eval("var_dump($v);"); err != nil {
-        t.Errorf("TestArgs: %s", err)
-    }
+	if err := php.Eval("var_dump($v);"); err != nil {
+		t.Errorf("TestArgs: %s", err)
+	}
 }
 
 func TestUnset(t *testing.T) {
-    php.Unset("v")
+	php.Unset("v")
 
-    if err := php.Eval("var_dump($v);"); err == nil {
-        t.Error("php.Eval should have a panic.")
-    }
+	if err := php.Eval("var_dump($v);"); err == nil {
+		t.Error("php.Eval should have a panic.")
+	}
 }
 
 func TestScope(t *testing.T) {
-    if err := php.Eval("$v = 'hello world';"); err != nil {
-        t.Error("TestScope: %s", err)
-    }
+	if err := php.Eval("$v = 'hello world';"); err != nil {
+		t.Error("TestScope: %s", err)
+	}
 
-    if err := php.Eval("var_dump($v);"); err != nil {
-        t.Error("TestScope: %s", err)
-    }
+	if err := php.Eval("var_dump($v);"); err != nil {
+		t.Error("TestScope: %s", err)
+	}
 }
 
 func TestScopeExec(t *testing.T) {
-    if err := php.Eval("$v = 'hello world';"); err != nil {
-        t.Error("TestScope: %s", err)
-    }
+	if err := php.Eval("$v = 'hello world';"); err != nil {
+		t.Error("TestScope: %s", err)
+	}
 
-    if err := ioutil.WriteFile(FileName, []byte("<?php echo $v;"), 0644); err != nil {
-        t.Errorf("ioutil.WriteFile: %s", err)
-    }
-    defer func() {
-        if err := os.Remove(FileName); err != nil {
-            t.Errorf("os.Remove: %s", err)
-        }
-    }()
-    if err := php.Exec(FileName); err != nil {
-        t.Errorf("php.Exec: %s", err)
-    }
+	if err := ioutil.WriteFile(FileName, []byte("<?php echo $v;"), 0644); err != nil {
+		t.Errorf("ioutil.WriteFile: %s", err)
+	}
+	defer func() {
+		if err := os.Remove(FileName); err != nil {
+			t.Errorf("os.Remove: %s", err)
+		}
+	}()
+	if err := php.Exec(FileName); err != nil {
+		t.Errorf("php.Exec: %s", err)
+	}
 }
