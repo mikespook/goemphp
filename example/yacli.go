12ff4e04b5d770d95e93874749ebbaa968ba6926
Xing Xing <mikespook@gmail.com>
2015-09-04 12:51:16 +1200

    go fmt

--- example/yacli.go
+++ example/yacli.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-    "log"
-    "flag"
-    "github.com/mikespook/goemphp/php"
+	"flag"
+	"log"
+
+	"github.com/mikespook/goemphp"
 )
 
 var (
-    info = flag.Bool("i", false, "Display phpinfo")
-    file = flag.String("f", "", "Execute the file")
-    run = flag.String("r", "", "Execute the script")
+	info = flag.Bool("i", false, "Display phpinfo")
+	file = flag.String("f", "", "Execute the file")
+	run  = flag.String("r", "", "Execute the script")
 )
 
 func init() {
-    flag.Parse()
+	flag.Parse()
 }
 
 func main() {
-    p := php.New()
-    p.Startup()
-    defer p.Close()
-    if *info {
-        if err := p.Eval("phpinfo();"); err != nil {
-            log.Fatal(err)
-        }
-        return
-    }
-    if *file != "" {
-        if err := p.Exec(*file); err != nil {
-            log.Fatal(err)
-        }
-        return
-    }
-    if *run != "" {
-        if err := p.Eval(*run); err != nil {
-            log.Fatal(err)
-        }
-        return
-    }
-    flag.Usage()
+	p := php.New()
+	p.Startup()
+	defer p.Close()
+	if *info {
+		if err := p.Eval("phpinfo();"); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	if *file != "" {
+		if err := p.Exec(*file); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	if *run != "" {
+		if err := p.Eval(*run); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	flag.Usage()
 }
